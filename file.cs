28263dabb55285d698adbf8c913e34abd035390d
MrSantiaga <mr.santiaga@gmail.com>
2026-05-25 19:41:00 +0300

    Change file tabs

--- file.cs
+++ file.cs
@@ -2,9 +2,9 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		Console.WriteLine("Enter your name:");
-		var name = Console.ReadKey();
-		Console.Write("Hello, world");
-		Console.WriteLine(name);
+			Console.WriteLine("Enter your name:");
+			var name = Console.ReadKey();
+			Console.Write("Hello, world");
+			Console.WriteLine(name);
 	}
 }
