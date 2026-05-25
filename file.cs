b5f69556fcbf90ea145406daee691b18dba5c7c8
MrSantiaga <mr.santiaga@gmail.com>
2026-05-25 19:28:44 +0300

    Intial commit

---
+++ file.cs
@@ -0,0 +1,10 @@
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Enter your name:");
+		var name = Console.ReadKey();
+		Console.Write("Hello, ");
+		Console.WriteLine(name);
+	}
+}
