de27bb7775481b680c3ec7f27b94571a8066e643
MrSantiaga <mr.santiaga@gmail.com>
2026-05-25 19:42:23 +0300

    Critical changes

--- file.cs
+++ file.cs
@@ -2,9 +2,8 @@ class Program
 {
 	static void Main(string[] args)
 	{
-			Console.WriteLine("Enter your name:");
+			Console.WriteLine("Enter somesthing:");
 			var name = Console.ReadKey();
-			Console.Write("Hello, world");
 			Console.WriteLine(name);
 	}
 }
